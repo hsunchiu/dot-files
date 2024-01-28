81a0c20bec48b78479499440ccb0747d25b51163
Hsun Chiu <hsun@Hsuns-Air.lan>
2024-01-29 04:48:50 +0800

    add program mode

--- emacs/init.el
+++ emacs/init.el
@@ -1,23 +1,59 @@
-;; In China, the network is blocked by GFW
-(setq package-archives '(
-                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+;;; init.el --- emacs config
+;;; Commentary:
+;;;
+
+;; In China, the network is blocked by GFW, I use TUNA.
+(setq package-archives '(("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
                           ("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                           ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-                          ("nogpu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-                          ))
-
+                          ("nogpu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")))
+;; (add-to-list 'package-archives '("melpa" . "xxxx"))
 (package-initialize)
 
+;; Settings for company, auto-complete only for coding.
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'company-mode))
+
+;; Flymake, syntax check
+(use-package flymake
+  :hook (prog-mode . flymake-mode)
+  :init
+  (add-hook 'prog-mode-hook 'flymake-mode)
+  :bind (("M-n" . #'flymake-goto-next-error)
+	 ("M-p" . #'flymake-goto-prev-error)))
+
+;; Format, don't using lsp
+;; use ditorconfig to format project
+(use-package editorconfig
+  :ensure t
+  :init
+  (add-hook 'prog-mode 'editorconfig-mode))
+
+;; auto install treesit libs
+;; `treesit-auto-install-all`
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
+
+;; elgot for lsp client
+(use-package eglot
+  :init
+  (add-hook 'prog-mode-hook #'eglot-ensure))
+
+;; C/C++ lsp-server: clangd (`brew install llvm` in macos)
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode))
+
+;;TODO: Rust lsp-server: rust-analyzer
+
+;; show keymapping to hint
 (use-package which-key
   :ensure t
   :init
-  (which-key-mode)
-  )
-
-(use-package editorconfig
-  :ensure t
-  :config
-  (editorconfig-mode 1))
+  (which-key-mode))
 
 ;; https://book.emacs-china.org/#org1ebaa5d
 ;; easy tempo
@@ -27,3 +63,8 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+
+
+
+
+;;; init.el
